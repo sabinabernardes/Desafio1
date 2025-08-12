7743866e6d2c86e0d0438de44a911ca246e5745e
Sabina Bernardes <66752747+sabinabernardes@users.noreply.github.com>
2025-08-12 14:57:18 -0300

    Add project configuration files and update dependencies

--- app/src/main/java/com/picpay/desafio/android/UserListItemViewHolder.kt
+++ app/src/main/java/com/picpay/desafio/android/UserListItemViewHolder.kt
@@ -1,29 +1,35 @@
 package com.picpay.desafio.android
 
 import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.list_item_user.view.*
 
 class UserListItemViewHolder(
     itemView: View
 ) : RecyclerView.ViewHolder(itemView) {
+    private val name: TextView = itemView.findViewById(R.id.name)
+    private val username: TextView = itemView.findViewById(R.id.username)
+    private val progressBar: ProgressBar = itemView.findViewById(R.id.progressBar)
+    private val picture: ImageView = itemView.findViewById(R.id.picture)
 
     fun bind(user: User) {
-        itemView.name.text = user.name
-        itemView.username.text = user.username
-        itemView.progressBar.visibility = View.VISIBLE
+        name.text = user.name
+        username.text = user.username
+        progressBar.visibility = View.VISIBLE
         Picasso.get()
             .load(user.img)
             .error(R.drawable.ic_round_account_circle)
-            .into(itemView.picture, object : Callback {
+            .into(picture, object : Callback {
                 override fun onSuccess() {
-                    itemView.progressBar.visibility = View.GONE
+                    progressBar.visibility = View.GONE
                 }
 
                 override fun onError(e: Exception?) {
-                    itemView.progressBar.visibility = View.GONE
+                    progressBar.visibility = View.GONE
                 }
             })
     }
